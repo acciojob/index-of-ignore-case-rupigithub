5af15d5553736d75419bf8cc243bd943566574de
rupigithub <117827907+rupigithub@users.noreply.github.com>
2024-01-17 14:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s2.toLowercase();
+	let l = s1.length;
+	for(let i=0; i<l i++){
+		if(s1.charAt(i) === s2.charAt(0)){
+			console.log(i);
+		}
+	}
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
